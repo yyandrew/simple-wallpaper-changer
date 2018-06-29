482315db6fb619a28372c0b980ea3efd6e205ebb
lq201 <zzcv20051122@gmail.com>
2018-06-29 08:01:07 +0800

    MISC - 格式化代码

--- main.go
+++ main.go
@@ -10,7 +10,14 @@ import (
 	"strings"
 )
 
-func detectDesktopEnv() string {
+// DetectDesktopEnv returns which Desktop environment you are using.It should return one of:
+// Unity
+// LXDE
+// GNOME
+// KDE
+// MATE
+// XFCE
+func DetectDesktopEnv() string {
 	var out bytes.Buffer
 	cmd := exec.Command("/bin/sh", "-c", "ls /usr/bin | grep session")
 	cmd.Stdout = &out
@@ -29,36 +36,39 @@ func detectDesktopEnv() string {
 	}
 }
 
-func main() {
-	var err error
+// ChangeWallPaper returns an error
+//
+// It will change you wall paper
+func ChangeWallPaper(file string) error {
 	var cmd *exec.Cmd
-	var out bytes.Buffer
 	osName := runtime.GOOS
-	imagesDir := flag.String("d", "", "Select you image folder")
-	file := flag.String("f", "", "Give a image file")
-	flag.Parse()
-	if *file == "" {
-		log.Fatal("you must give a image by -f options")
-	}
-	fmt.Println(*imagesDir)
 	if osName == "linux" {
-		// /usr/share/lubuntu/wallpapers/formula_1_ferrari_f2008-wallpaper-1366x768.jpg
-		// /home/andrew/Pictures/desktop 1_001.png
-		switch detectDesktopEnv() {
+		switch DetectDesktopEnv() {
 		case "LXDE":
-			cmd = exec.Command("pcmanfm", "-w", *file)
+			cmd = exec.Command("pcmanfm", "-w", file)
 		case "GNOME", "Unity":
-			cmd = exec.Command("gsettings", "set", "org.gnome.desktop.background", "picture-uri", "file://"+*file)
+			cmd = exec.Command("gsettings", "set", "org.gnome.desktop.background", "picture-uri", "file://"+file)
 		default:
 			log.Fatal("Not support this desktop environment!")
 		}
 	} else if osName == "darwin" {
-		cmd = exec.Command("/usr/bin/osascript", "-e", fmt.Sprintf(`tell application "Finder" to set desktop picture to POSIX file "%s"`, *file))
+		cmd = exec.Command("/usr/bin/osascript", "-e", fmt.Sprintf(`tell application "Finder" to set desktop picture to POSIX file "%s"`, file))
 	}
-	err = cmd.Run()
-	if err != nil {
-		log.Fatal(err)
+	if err := cmd.Run(); err != nil {
+		return err
 	}
+	return nil
+}
 
-	fmt.Printf("%q \n", out.String())
+func main() {
+	imagesDir := flag.String("d", "", "Select you image folder")
+	file := flag.String("f", "", "Give a image file")
+	flag.Parse()
+	if *file == "" {
+		log.Fatal("You must give a image by -f options")
+	}
+	fmt.Println(*imagesDir)
+	if err := ChangeWallPaper(*file); err != nil {
+		log.Fatal(err.Error())
+	}
 }
